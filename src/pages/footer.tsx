e5b4b882f7134005d9b59b40db2ce1102100a541
ferdous725890 <ferdous725890@gmail.com>
2025-08-02 23:56:55 -0700

    Local changes

--- src/pages/footer.tsx
+++ src/pages/footer.tsx
@@ -2,8 +2,8 @@
 export default function Footer() {
   return (
     <div>
-<h4 className="">
-    Task Manage Footer Section</h4>      
+      <h4 className="">
+        Task Manage Footer Section</h4>
     </div>
   )
 }
--- src/pages/navbar.tsx
+++ src/pages/navbar.tsx
@@ -1,16 +1,25 @@
+import { ModeToggle } from "@/components/ui/mode-toggle";
 import { Link } from "react-router";
 
 export default function Navbar() {
-    return <div className="bg-yellow-500 p-2 rounded-md ">
-    <ul className="flex space-x-4 font-bold ">
-        <li> 
-            <Link className="" to={'/user'}>User</Link>
+  return (
+    <div className="bg-yellow-500 p-2 rounded-md ">
+      <ul className="flex space-x-4 font-bold ">
+        <li>
+          <Link className="" to={"/user"}>
+            User
+          </Link>
         </li>
         <li>
-
-            <Link className="" to={'/task'}>Task</Link>
+          <Link className="" to={"/task"}>
+            Task
+          </Link>
         </li>
-    </ul>
+
+        <div>
+          <ModeToggle></ModeToggle>
+        </div>
+      </ul>
     </div>
-    
+  );
 }
